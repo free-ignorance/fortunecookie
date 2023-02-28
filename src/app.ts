98d2ffd5df4e86554685c26140cf6d04b1a729e4
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-02-28 12:02:28 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,21 @@ class App {
     this.app.use(helmet());
     this.app.use(compression());
     this.app.use(bodyParser.json());
+    
+    /** set up cors middleware
+     * @param {Request} req - Express request object
+     * @param {Response} res - Express response object
+     * @param {Next} next - Express Next object
+     */
+    this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Accept, Authorization, Content-Type, Origin, X-Requested-With"
+      );
+      res.header("Access-Control-Allow-Methods", "GET");
+      next();
+    });
   }
 
   private initializeControllers(controllers: Controller[]) {
