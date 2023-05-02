ddda0e0d59640665ae19f24791f4b09a240de8bd
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-05-02 12:59:23 -0400

    Update slack.ts

--- src/controllers/slack.ts
+++ src/controllers/slack.ts
@@ -18,7 +18,7 @@ class SlackController extends DefaultController {
   ) => {
     response.status(200).send({
       response_type: "in_channel",
-      text: getFortune(),
+      text: "🥠 your fortune reads: '" + getFortune()  + "'",
     });
   };
 }
