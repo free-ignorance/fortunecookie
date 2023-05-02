874d37f5be6122679949c8201d7666af5d7ab832
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-05-02 12:28:10 -0400

    feat: added slack response (#316)
    * feat: added slack response
    
    * fix: lint

---
+++ src/controllers/slack.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import { DefaultController } from "./";
+import { getFortune } from "../models/fortune";
+
+class SlackController extends DefaultController {
+  constructor() {
+    super("/slack");
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(this.path, this.getSlackResponseRandom);
+  }
+
+  private getSlackResponseRandom = (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    response
+      .status(200)
+      .send({ response_type: "in_channel", text: `${getFortune()}` });
+  };
+}
+
+export default SlackController;
