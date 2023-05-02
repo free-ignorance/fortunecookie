560f9577e4af3097640bbf07075d6bdcbb4c5459
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-05-02 12:29:56 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import App from "./app";
 
 import HealthCheckController from "./controllers/health";
 import MainController from "./controllers/main";
+import SlackController from "./controllers/slack";
+
 
 const app = new App([new HealthCheckController(), new MainController()]);
 
